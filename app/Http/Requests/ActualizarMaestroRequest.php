bd625e139d63422e507487d91395a168da4a565f
Martín Calderón <elmcwl@gmail.com>
2022-11-19 19:45:51 -0700

    añadidos componentes necesarios

---
+++ app/Http/Requests/ActualizarMaestroRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ActualizarMaestroRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'nombre' => "required",
+            'correo' => "required|unique:maestros,correo,".$this->route('maestro')->id,
+            'contraseña' => "required",
+            'estado_cuenta' => "required"
+        ];
+    }
+}
