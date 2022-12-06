25d4b51416629842f74033f209d970f48d740ae3
Martín Calderón <elmcwl@gmail.com>
2022-12-06 00:51:02 -0700

    login/registro2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -147,8 +147,8 @@ body {
     <h1>Cover your page.</h1>
     <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
     <p class="lead">
-      <a href="login.blade.php" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Iniciar sesión</a>
-      <a href="register.blade.php" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Registrarse</a>
+      <a href="/inicio_sesion" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Iniciar sesión</a>
+      <a href="/registro" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Registrarse</a>
     </p>
   </main>
 
