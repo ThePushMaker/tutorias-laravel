4065bb018c31a074c97b66fa5c5453669ff503d2
BRodriguez-18 <brodriguez_18@alu.uabcs.mx>
2022-12-06 02:22:17 -0700

    login y register

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -129,23 +129,23 @@ body {
     <!-- Custom styles for this template -->
     <link href="cover.css" rel="stylesheet">
   </head>
-  <body class="d-flex h-100 text-center text-bg-dark">
+  <body class="d-flex h-100 text-center " style="background-color:#002064; color: white;">
     
 <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
   <header class="mb-auto">
     <div>
       <h3 class="float-md-start mb-0">Cover</h3>
       <nav class="nav nav-masthead justify-content-center float-md-end">
-        <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="#">Home</a>
-        <a class="nav-link fw-bold py-1 px-0" href="#">Features</a>
-        <a class="nav-link fw-bold py-1 px-0" href="#">Contact</a>
+        <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="#">Alumno</a>
+        <a class="nav-link fw-bold py-1 px-0" href="#">Tutor</a>
+        <a class="nav-link fw-bold py-1 px-0" href="#">Profesor</a>
       </nav>
     </div>
   </header>
 
   <main class="px-3">
-    <h1>Cover your page.</h1>
-    <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+    <h1>Bienvenido a stdasc.</h1>
+    <p class="lead">stdasc es el programa de tutorias de alumnos para los alumnis.</p>
     <p class="lead">
       <a href="/inicio_sesion" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Iniciar sesión</a>
       <a href="/registro" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Registrarse</a>
@@ -153,7 +153,7 @@ body {
   </main>
 
   <footer class="mt-auto text-white-50">
-    <p>Cover template for <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
+    <p>Universida autonoma de baja california sur</p>
   </footer>
 </div>
 
--- resources/views/inicio_sesion.blade.php
+++ resources/views/inicio_sesion.blade.php
@@ -1,13 +1,89 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>Iniciar Sesión</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+
+    <!-- Bootstrap v5.1.3 CDNs -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+
+  
+
 </head>
+
 <body>
-   
-    login 
+
+    <div class="login">
+
+        <h1 class="text-center">Bienvenido!</h1>
+        
+        <form class="needs-validation">
+            <div class="form-group was-validated">
+                <label class="form-label" for="email">Correo electronico</label>
+                <input class="form-control" type="email" id="email" required>
+                <div class="invalid-feedback">
+                    Porfavor ingrese su correo electronico
+                </div>
+            </div>
+            <div class="form-group was-validated">
+                <label class="form-label" for="password">Contraseña</label>
+                <input class="form-control" type="password" id="password" required>
+                <div class="invalid-feedback">
+                    Porfavor ingrese su contraseña
+                </div>
+            </div>
+            <!-- <div class="form-group form-check">
+                <input class="form-check-input" type="checkbox" id="check">
+                <label class="form-check-label" for="check">Remember me</label>
+            </div> -->
+            <input class="btn btn-success w-100" type="submit" value="SIGN IN">
+        </form>
+
+    </div>
+
 </body>
+<style>
+    * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body {
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: #002064;
+}
+
+.login {
+    width: 360px;
+    height: min-content;
+    padding: 20px;
+    border-radius: 12px;
+    background: #fff;
+}
+
+.login h1 {
+    font-size: 36px;
+    margin-bottom: 25px;
+}
+
+.login form {
+    font-size: 20px;
+}
+
+.login form .form-group {
+    margin-bottom: 12px;
+}
+
+.login form input[type="submit"] {
+    font-size: 20px;
+    margin-top: 15px;
+}
+</style>
 </html>
